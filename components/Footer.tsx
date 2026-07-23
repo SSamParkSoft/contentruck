eb6d43d20b010a3fac9c7ff5da08a18586e71123
pji0401 <pji040111@naver.com>
2026-07-23 17:59:25 +0900

    푸터 사업자 정보 추가

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -9,6 +9,18 @@ export function Footer() {
         <span>© 2026 Contentruck. All rights reserved.</span>
         <div><span aria-disabled="true">개인정보처리방침</span><span aria-disabled="true">이용약관</span></div>
       </div>
+      <div className="container footer-business">
+        <strong>주식회사 콘텐트럭</strong>
+        <div className="footer-business-meta">
+          <span>대표자: 박종인</span>
+          <span>사업자등록번호: 614-87-03791</span>
+          <a href="mailto:ceo@contentruck.co.kr">이메일: ceo@contentruck.co.kr</a>
+        </div>
+        <address>
+          주소: 경기도 안산시 상록구 한양대학로 55, 제5공학관 지하 1층{" "}
+          <span className="footer-nowrap">소프트웨어창업실</span> 2호
+        </address>
+      </div>
     </footer>
   );
 }
